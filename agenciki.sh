761d4f9d8affeea2100244a7d1f3576281e355da
awry1 <92302998+awry1@users.noreply.github.com>
2024-10-22 03:29:23 +0200

    fixes and stuff

--- agenciki.sh
+++ agenciki.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Blah blah star topology or sum sheit script
+used_ports=()   # array of used ports
 
 problem() {
     echo "⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝"
@@ -24,37 +25,28 @@ get_input() {
     # $2 - star (y/n)
     # $3 - encr (y/n)
     # $4 - auth (y/n)
-    # $5 - gui/container
+    # $5 - gui/backupmain/container
+    # $6 - name
+    # $7 - host
+    # $8 - port
 
     echo -e "\n--- $1 Container ---"
 
-    echo -n "Platform name: "
-    read name
-    echo -n  "Container name: "
-    read container_name
-    echo -n  "Host: "
-    read host
-    echo -n  "Port: "
-    read port
-    echo -n  "Local host: "
-    read local_host
-    echo -n  "Local port: "
-    read local_port
+    read -p "Container name: " container_name
+    read -p "Local host: " local_host
+    read -p "Local port: " local_port
 
     command="java"
     if [ "$4" == "y" ]; then  # encr
-        echo -n "Keystore name: "
-        read keystore
+        read -p "Keystore name: " keystore
         if [ -z "$keystore" ]; then
             problem "keystore"
         fi
-        echo -n "Keystore password: "
-        read password
+        read -p "Password: " password
         if [ -z "$password" ]; then
             problem "password"
         fi
-        echo -n "Keystore2 name: "
-        read keystore2
+        read -p "Keystore2 name: " keystore2
         if [ -z "$keystore2" ]; then
             problem "keystore2"
         fi
@@ -66,37 +58,49 @@ get_input() {
         # Import public key
         keytool -import -file "$keystore".cer -alias "$keystore" -keystore "$keystore2"-ca.jks
 
-        command="$command -Djavax.net.ssl.keyStore=${keystore}.jks\
-        -Djavax.net.ssl.keyStorePassword=${password}\
-        -Djavax.net.ssl.trustStore=${keystore}-ca.jks"
+        command="$command -Djavax.net.ssl.keyStore=${keystore}.jks"
+        command="$command -Djavax.net.ssl.keyStorePassword=${password}"
+        command="$command -Djavax.net.ssl.trustStore=${keystore2}-ca.jks"
     fi
     command="$command -cp jade.jar jade.Boot -$5"
-    if [ -n "$name" ]; then
-        command="$command -name $name"
+    if [ -n "$6" ]; then
+        command="$command -name $6"
+    fi
+    if [ -n "$7" ]; then
+        command="$command -host $7"
+    fi
+    if [ -n "$8" ]; then
+        command="$command -port $8"
+        base_port="$8"
+    else
+        base_port=1099
     fi
     if [ -n "$container_name" ]; then
         command="$command -container-name $container_name"
-    fi
-    if [ -n "$host" ]; then
-        command="$command -host $host"
-    fi
-    if [ -n "$port" ]; then
-        command="$command -port $port"
     fi
     if [ -n "$local_host" ]; then
         command="$command -local-host $local_host"
     fi
     if [ -n "$local_port" ]; then
         command="$command -local-port $local_port"
+        used_ports+=("$local_port")
+        base_port=$((local_port+1))
+    else
+        while [[ " ${used_ports[@]} " =~ " ${base_port} " ]]; do
+            base_port=$((base_port+1))
+        done
+        command="$command -local-port $base_port"
+        used_ports+=("$base_port")
+        base_port=$((base_port+1))
     fi
     if [ "$2" == "y" ]; then  # star
         command="$command -services"
         if [ "$1" == "Main" ]; then
-            command="$command jade.core.replication.MainReplicationService\;\
-            jade.core.replication.AddressNotificationService"
+            command="$command jade.core.replication.MainReplicationService\;"
+            command="$command jade.core.replication.AddressNotificationService"
         elif [ "$1" == "Backup" ]; then
-            command="$command jade.core.replication.MainReplicationService\;\
-            jade.core.replication.AddressNotificationService"
+            command="$command jade.core.replication.MainReplicationService\;"
+            command="$command jade.core.replication.AddressNotificationService"
         elif [ "$1" == "Federated" ]; then
             command="$command jade.core.replication.AddressNotificationService"
         fi
@@ -107,19 +111,22 @@ get_input() {
 }
 
 start_jade() {
-    type="$1"
+    mode="$1"
     star="$2"
     encr="$3"
     auth="$4"
+    name="$5"
+    host="$6"
+    port="$7"
 
-    if [ -z "$type" ]; then        # all
-        get_input "Main" "$star" "$encr" "$auth" "gui"
+    if [ -z "$mode" ]; then        # all
+        get_input "Main" "$star" "$encr" "$auth" "gui" "$name" "$host" "$port"
         command_gui=$command
         if [ "$star" == "y" ]; then
-            get_input "Backup" "$star" "$encr" "$auth" "container"
+            get_input "Backup" "$star" "$encr" "$auth" "backupmain" "$name" "$host" "$port"
             command_backup=$command
         fi
-        get_input "Federated" "$star" "$encr" "$auth" "container"
+        get_input "Federated" "$star" "$encr" "$auth" "container" "$name" "$host" "$port"
         command_container=$command
 
         gnome-terminal -- bash -c "$command_gui; exec bash"
@@ -128,15 +135,15 @@ start_jade() {
         fi
         gnome-terminal -- bash -c "$command_container; exec bash"
     
-    elif [ "$type" == "0" ]; then  # main
-        get_input "Main" "$star" "$encr" "$auth" "gui"
+    elif [ "$mode" == "0" ]; then  # main
+        get_input "Main" "$star" "$encr" "$auth" "gui" "$name" "$host" "$port"
         command_gui=$command
 
         gnome-terminal -- bash -c "$command_gui; exec bash"
     
-    elif [ "$type" == "1" ]; then  # backup
+    elif [ "$mode" == "1" ]; then  # backup
         if [ "$star" == "y" ]; then
-            get_input "Backup" "$star" "$encr" "$auth" "backupmain"
+            get_input "Backup" "$star" "$encr" "$auth" "backupmain" "$name" "$host" "$port"
             command_backup=$command
 
             gnome-terminal -- bash -c "$command_backup; exec bash"
@@ -145,37 +152,47 @@ start_jade() {
             exit 1
         fi
     
-    elif [ "$type" == "2" ]; then  # federated
-        get_input "Federated" "$star" "$encr" "$auth" "container"
+    elif [ "$mode" == "2" ]; then  # federated
+        get_input "Federated" "$star" "$encr" "$auth" "container" "$name" "$host" "$port"
         command_container=$command
 
         gnome-terminal -- bash -c "$command_container; exec bash"
     fi
 }
 
-# Main
-if [ -n "$1" ] && [ "$1" -gt 2 ]; then
-    echo "Unknown argument (none/0/1/2)"
-    exit 1
-fi
+main () {
+    if [ -n "$1" ] && [ "$1" -gt 2 ]; then
+        echo "Unknown argument (none/0/1/2)"
+        exit 1
+    fi
 
-echo -n "Star topology (y/n): "
-read star
-if [ -z "$star" ]; then
-    star="n"
-fi
-echo -n "Encryption (y/n): "
-read encr
-if [ -z "$encr" ]; then
-    encr="n"
-fi
-echo -n "Authentication (y/n): "
-read auth
-if [ -z "$auth" ]; then
-    auth="n"
-fi
+    read -p "Platform name: " name
+    read -p "Host: " host
+    read -p "Port: " port
+    read -p "Star topology (y/n): " star
+    if [ -z "$star" ]; then
+        star="n"
+        echo -e "\033[1A\033[2K\rStar topology (y/n): n"    # Such wow, much magic
+    fi
+    read -p "Encryption (y/n): " encr
+    if [ -z "$encr" ]; then
+        encr="n"
+        echo -e "\033[1A\033[2K\rEncryption (y/n): n"
+    fi
+    read -p "Authentication (y/n): " auth
+    if [ -z "$auth" ]; then
+        auth="n"
+        echo -e "\033[1A\033[2K\rAuthentication (y/n): n"
+    fi
 
-start_jade "$1" "$star" "$encr" "$auth"
+    start_jade "$1" "$star" "$encr" "$auth" "$name" "$host" "$port"
+}
 
+main "$1"
 echo -e "\nElo Żelo"
 exit 0
+
+# To Do:
+# fix encr
+# fix auth
+# optional (stupid idea): used_ports in tmp file
